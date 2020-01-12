a03da47f047e85adf14e3c15459dbcccd5f8caf1
mittulmandhan <conanmittul@gmail.com>
2020-01-12 21:47:21 +0530

    default access modifiers

---
+++ src/main/java/com/defaultdemo/A.java
@@ -0,0 +1,15 @@
+package com.defaultdemo;
+
+
+//INSIDE PACKAGE
+//within same package, default behaves as public (Oneliner)
+
+//OUTSIDE PACKAGE
+//outside package, default acts as private (Oneliner)
+
+
+
+public class A {
+	// default access level variable
+	int x=10;
+}
---
+++ src/main/java/com/defaultdemooutside/C.java
@@ -0,0 +1,11 @@
+package com.defaultdemooutside;
+
+import com.defaultdemo.A;
+
+public class C {
+
+	
+	public static void main(String[] args) {
+		//new A().x;
+	}
+}

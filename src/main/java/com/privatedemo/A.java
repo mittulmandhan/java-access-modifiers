44fbd8148e89f2b5d16852384fe6546e150e7782
mittulmandhan <conanmittul@gmail.com>
2020-01-12 21:49:01 +0530

    private access modifier

---
+++ src/main/java/com/privatedemo/A.java
@@ -0,0 +1,17 @@
+package com.privatedemo;
+
+//resourse = class + (variables + methods)
+//resourse = class + members
+public class A {
+	private int x;
+
+	private void ab() {
+		this.x = 10;// x is visible inside class
+	}
+
+	// public static void main(String args[]) {
+	// A obj=new A();
+	// obj
+	// System.out.println(obj.x);
+	// }
+}
---
+++ src/main/java/com/privatedemo/B.java
@@ -0,0 +1,8 @@
+package com.privatedemo;
+
+public class B {
+	void my() {
+		//new A().x;// private var is not accesible outside class
+		//new A().ab();// private var is not accesible outside class
+	}
+}
